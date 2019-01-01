d804df434032aa50eb830bc2aadc4b2f28bc1f75
root <root@localhost.localdomain>
2019-01-01 15:48:10 -0600

    added db files

--- Ecobee_influx.py
+++ Ecobee_influx.py
@@ -7,6 +7,8 @@ from six.moves import input
 
 from pyecobee import *
 
+import simplejson as json
+
 from influxdb import InfluxDBClient
 
 import logging
@@ -15,7 +17,7 @@ logger = logging.getLogger(__name__)
 
 # create a file handler
 handler = logging.FileHandler('ecobee_influx.log')
-handler.setLevel(logging.INFO)
+handler.setLevel(logging.DEBUG)
 
 # create a logging format
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -24,9 +26,15 @@ handler.setFormatter(formatter)
 # add the handlers to the logger
 logger.addHandler(handler)
 
-
+#files for persistance storage 
 file_name = 'pyecobee_db'
 
+# influx db information
+influxhost = '192.168.0.101'
+influxport = '8086'
+influxdbname = 'ecobee'
+
+#grab data from persistance storage
 
 def persist_to_shelf(file_name, ecobee_service):
     pyecobee_db = shelve.open(file_name, protocol=2)
@@ -91,41 +99,49 @@ if __name__ == '__main__':
     elif now_utc > ecobee_service.access_token_expires_on:
         token_response = ecobee_service.refresh_tokens()
 
+influx_client = InfluxDBClient(host=influxhost, port=influxport, database=influxdbname)
 
-while true
+while True:
 
     try:
-        thermostat_summary_response = ecobee_service.request_thermostats_summary(selection=Selection(
+        thermostat_response = ecobee_service.request_thermostats(selection=Selection(
         selection_type=SelectionType.REGISTERED.value,
         selection_match='',
-        include_equipment_status=True,
-        include_alerts=True,
-        include_device=True, 
-        include_electricity=False, 
-        include_equipment_status=True,
-        include_events=True, 
-        include_extended_runtime=True, 
-        include_house_details=True,
-        include_location=True, 
-        include_management=False, 
-        include_notification_settings=True,
-        include_oem_cfg=False, 
-        include_privacy=False, 
-        include_program=True, 
-        include_reminders=False,
-        include_runtime=True, 
-        include_security_settings=False, 
-        include_sensors=True,
-        include_settings=True, 
-        include_technician=False, 
-        include_utility=False, 
-        include_version=False,
-        include_weather=False))
-
+#        include_equipment_status=True,
+#        include_alerts=True,
+#        include_device=True, 
+#        include_electricity=False, 
+#        include_events=True, 
+        include_extended_runtime=True 
+#        include_house_details=True,
+#        include_location=True, 
+#        include_management=False, 
+#        include_notification_settings=True,
+#        include_oem_cfg=False, 
+#        include_privacy=False, 
+#        include_program=True, 
+#        include_reminders=False,
+#        include_runtime=True, 
+#        include_security_settings=False, 
+#        include_sensors=True,
+#        include_settings=True, 
+#        include_technician=False, 
+#        include_utility=False, 
+#        include_version=False,
+#        include_weather=False
+	))
     except EcobeeApiException as e:
         if e.status_code == 14:
             token_response = ecobee_service.refresh_tokens()
-    thermostat_summary_response
+ 
+    logger.info(thermostat_response.pretty_format())
+    assert thermostat_response.status.code == 0, 'Failure while executing request_thermostats:\n{0}'.format(    
+      	   thermostat_response.pretty_format())
+
+#    influx_client.write_points(thermostat_response.pretty_format())
+#    parsed_response = json.loads(thermostat_response.pretty_format())
+#    logger.info(parsed_response['thermostatList'])
+#    print(thermostat_response.extendedRuntime())
     time.sleep(300)
 
 
